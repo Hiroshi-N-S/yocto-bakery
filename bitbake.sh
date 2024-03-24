aec4395fadf30d72bd81867bea950be8346aad7c
Hiroshi.N.S <Hiroshi.N.September11@gmail.com>
2024-03-24 17:59:39 +0900

    feat: add script for bitbaking poky

---
+++ bitbake.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+BITBAKE_MACHINE=qemux86-64
+BITBAKE_TARGET=core-image-minimal
+BITBAKE_DEPLOY_DIR=$(cd ~; pwd)/build/tmp/deploy
+BITBAKE_RETRY_MAX=4
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+OUTPUT_DIR=${SCRIPT_DIR}/${DEST_DIR:-deploy}
+
+cd ~
+
+#
+# set up the development environment
+#
+source poky/oe-init-build-env build
+
+#
+# bitbake image
+#
+for i in $(seq ${BITBAKE_RETRY_MAX}) ; do \
+  bitbake ${BITBAKE_TARGET} && \
+  break
+  if [ $i -eq ${BITBAKE_RETRY_MAX} ] ; then exit 1 ; fi
+  sleep 1
+done
+
+# deploy artifacts
+mkdir -p ${OUTPUT_DIR}
+cp -r ~/build/conf ${OUTPUT_DIR}
+
+mkdir -p ${OUTPUT_DIR}/images/${BITBAKE_MACHINE}
+for file in $(ls -l ${BITBAKE_DEPLOY_DIR}/images/${BITBAKE_MACHINE} | grep '\->' | awk '{print $9}')
+do
+  cp ${BITBAKE_DEPLOY_DIR}/images/${BITBAKE_MACHINE}/${file} ${OUTPUT_DIR}/images/${BITBAKE_MACHINE}
+done
+
+#
+# bitbake sdk
+#
+for i in $(seq ${BITBAKE_RETRY_MAX}) ; do \
+  bitbake ${BITBAKE_TARGET} -c populate_sdk && \
+  break
+  if [ $i -eq ${BITBAKE_RETRY_MAX} ] ; then exit 1 ; fi
+  sleep 1
+done
+
+# deploy artifacts
+cp -r ${BITBAKE_DEPLOY_DIR}/sdk ${OUTPUT_DIR}/sdk
